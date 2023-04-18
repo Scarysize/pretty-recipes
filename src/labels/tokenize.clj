0158b1f1a399b17e7d74948c0c78af1b9cf2d583
Scarysize <Scarysize@user.noreply.github.com>
2023-04-18 19:08:51 +0200

    Fix tokenization on accented chars

--- src/labels/tokenize.clj
+++ src/labels/tokenize.clj
@@ -1,6 +1,6 @@
 (ns labels.tokenize)
 
-(def token-expr #"[\w\.\-\']+|\(|\)|,|\"")
+(def token-expr #"[a-zA-Z0-9_À-ÿ\.\-\']+|\(|\)|,|\"")
 
 (defn tokenize [phrase]
   (vec (re-seq token-expr phrase)))
