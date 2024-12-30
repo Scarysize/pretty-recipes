ed29a25195970c74452d6e00e4742d38cd381bb1
Scarysize <Scarysize@user.noreply.github.com>
2024-12-30 11:17:33 +0100

    Clean up unused code and notes

--- model-server/run.sh
+++
@@ -1,8 +0,0 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-pushd $SCRIPT_DIR
-echo "Installing deps..."
-/etc/poetry/bin/poetry install
-
-echo "Starting server..."
-/etc/poetry/bin/poetry run python model_server
